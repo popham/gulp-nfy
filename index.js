7895eac2fb7f180fd0ddb68bbdc20120f73fb9dc
Tim Popham <popham@uw.edu>
2014-12-01 13:18:05 -0800

    Migrate error messages to package name

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var path = require('path');
 module.exports = function () {
     var transform = function (file, enc, cb) {
         if (file.isNull()) return cb(null, file); 
-        if (file.isStream()) return cb(new gutil.PluginError('gulp-nodefy', 'Streaming not supported'));
+        if (file.isStream()) return cb(new gutil.PluginError('gulp-nfy', 'Streaming not supported'));
 
         var data;
         var str = file.contents.toString('utf8');
@@ -15,7 +15,7 @@ module.exports = function () {
         try {
             data = nfy(str);
         } catch (err) {
-            return cb(new gutil.PluginError('gulp-nodefy', err));
+            return cb(new gutil.PluginError('gulp-nfy', err));
         }
 
         file.contents = new Buffer(data);
